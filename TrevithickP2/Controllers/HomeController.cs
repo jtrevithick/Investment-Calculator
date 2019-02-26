1042b513977ccae1c4987b00bf841d817696cb79
JAMES TREVITHICK <jttjtrevithick@msn.com>
2019-02-26 13:22:27 -0700

    Importing Project

---
+++ TrevithickP2/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TrevithickP2.Models;
+
+namespace TrevithickP2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View(new InvestmentCalc());
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Summary(InvestmentCalc inv)
+        {
+            //ViewBag.Principle = inv.Principle;
+            return View();
+        }
+    }
+}
---
+++ TrevithickP2/Models/InvestmentCalc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrevithickP2.Models
+{
+    public class InvestmentCalc
+    {
+        public int cmpPerYr;
+        public double interest;
+        public double principle;
+        public int years;
+
+        public int CmpPerYr { get => cmpPerYr; set { Calc(); } }
+
+
+
+        public double FutureValue { get => FutureValue; set { Calc(); } }
+
+        public double Interest { get => interest; set { Calc(); } }
+
+        public double Principle { get => principle; set { Calc(); } }
+
+        public int Years { get=>years; set { Calc(); } }
+
+        public void Calc() {
+            FutureValue = principle*(1+(interest/cmpPerYr))*years;
+
+        }
+
+        public InvestmentCalc() {
+        }
+
+       public InvestmentCalc(double principle, int years, double interest, int numTimes) {
+            Principle = principle;
+            Years = years;
+            Interest = interest;
+            cmpPerYr = numTimes;
+        }
+    }
+}
